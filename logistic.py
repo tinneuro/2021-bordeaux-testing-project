a30403f7db3f14b925d362dd48da0844b960070c
ASPP Student <student@localhost>
2021-08-24 16:22:00 +0200

    Model iterations and convergence test

--- logistic.py
+++ logistic.py
@@ -1,4 +1,12 @@
+import numpy as np 
 def logistic_map(x,r):
 	f = r*x*(1-x)
 	return f
-	
+
+def logistic_iterate(itr, x, r):
+	output = np.zeros(itr+1)
+	output[0] = x
+	for i in range(itr):
+		output[i+1] = logistic_map(output[i],r) 
+
+	return output
--- test_logistic.py
+++ test_logistic.py
@@ -1,10 +1,22 @@
 import pytest
 import logistic
 import math
+import numpy as np
 
+@pytest.mark.implementation
 @pytest.mark.parametrize(["x","r","expected"], [(0.1,2.2, 0.198)])
 def test_logistic_map(x,r,expected):
 	result = logistic.logistic_map(x,r)
 	assert math.isclose(result,expected)
+	
+@pytest.mark.behavior
+def test_converge():
+	itr = 100
+	r = 1.5
+
+	for ii in range(100):
+		
+		x = np.random.rand(1)
+		assert math.isclose(logistic.logistic_iterate(itr, x, r)[-1],1/3)
 	
 
